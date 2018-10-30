337e3ef48b7830112070aa8d01b2f77823d68f23
Priit Pärl <priitparl@gmail.com>
2018-10-30 20:57:27 +0200

    Database scripts and build

--- src/main/java/ee/sport/jim/JimAppApplication.java
+++ src/main/java/ee/sport/jim/JimAppApplication.java
@@ -3,9 +3,11 @@ package ee.sport.jim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaRepositories
 public class JimAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(JimAppApplication.class, args);
