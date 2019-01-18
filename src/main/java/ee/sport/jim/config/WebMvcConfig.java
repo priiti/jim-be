c826488969c73123ef4a8f281591335df0a6d1eb
Priit Pärl <priitparl@gmail.com>
2019-01-18 16:39:48 +0200

    Swagger access

--- src/main/java/ee/sport/jim/config/WebMvcConfig.java
+++ src/main/java/ee/sport/jim/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package ee.sport.jim.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			.allowedOrigins("*")
 			.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
 			.maxAge(MAX_AGE_SECS);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
 	}
 }
